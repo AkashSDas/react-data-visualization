af92fd35ee29e6c237e1bbed833f01c74be4ce4f
AkashSDas <aakashdas368@gmail.com>
2021-12-30 18:49:01 +0530

    create line cjhart using react chart js 2

--- pages/chartjs/index.tsx
+++ pages/chartjs/index.tsx
@@ -1,4 +1,4 @@
-import { Bar } from "react-chartjs-2";
+import { Bar, Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,15 +7,22 @@ import {
   Title,
   Tooltip,
   Legend,
+  PointElement,
+  LineElement,
 } from "chart.js";
+import * as faker from "faker";
+import { useEffect, useState } from "react";
 
+// Register chart js
 ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  PointElement,
+  LineElement
 );
 
 const IndexPage = () => {
@@ -23,29 +30,171 @@ const IndexPage = () => {
     <div>
       <h1>Cart.js</h1>
       <BarChart />
+      <LineChart />
     </div>
   );
 };
 
+// *****************
+// Bar Chart
+// *****************
+
 const BarChart = () => {
-  const x = [1, 2, 3, 4, 5, 6, 7, 8];
-  const y = [1, 2, 3, 4, 5, 6, 7, 8];
-  const dataset = {
-    labels: y,
+  const [xCount, setXCount] = useState(10);
+  const [data, setData] = useState({
+    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    y: [
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+    ],
+  });
+
+  const [dataset, setDataset] = useState({
+    labels: data.x,
     datasets: [
       {
         label: "Bar chart",
-        data: x,
+        data: data.y,
         backgroundColor: "#8c0ffa",
         borderRadius: 12,
-        grid: false,
       },
     ],
-  };
+  });
+
+  useEffect(() => {
+    setDataset({
+      labels: data.x,
+      datasets: [
+        {
+          label: "Bar chart",
+          data: data.y,
+          backgroundColor: "#8c0ffa",
+          borderRadius: 12,
+        },
+      ],
+    });
+  }, [data.x, data.y]);
 
   return (
     <div style={{ height: "600px", width: "600px" }}>
-      <Bar data={dataset} />
+      <Bar data={dataset} options={{ animation: { duration: 300 } }} redraw />
+      <button
+        onClick={() => {
+          setXCount((count) => count + 1);
+          setData({
+            x: [...data.x, xCount],
+            y: [...data.y, faker.datatype.number(100)],
+          });
+        }}
+      >
+        Add data
+      </button>
+      <button
+        onClick={() => {
+          setXCount((count) => count - 1);
+          let tmpX = [...data.x];
+          tmpX.splice(-1);
+          let tmpY = [...data.y];
+          tmpY.splice(-1);
+          setData({
+            x: [...tmpX],
+            y: [...tmpY],
+          });
+        }}
+      >
+        Remove last sample
+      </button>
+    </div>
+  );
+};
+
+// *****************
+// Line Chart
+// *****************
+
+const LineChart = () => {
+  const [xCount, setXCount] = useState(10);
+  const [data, setData] = useState({
+    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    y: [
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+      faker.datatype.number(100),
+    ],
+  });
+
+  const [dataset, setDataset] = useState({
+    labels: data.x,
+    datasets: [
+      {
+        label: "Bar chart",
+        data: data.y,
+        backgroundColor: "#8c0ffa",
+        borderRadius: 12,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setDataset({
+      labels: data.x,
+      datasets: [
+        {
+          label: "Bar chart",
+          data: data.y,
+          backgroundColor: "#8c0ffa",
+          borderRadius: 12,
+        },
+      ],
+    });
+  }, [data.x, data.y]);
+
+  return (
+    <div style={{ height: "600px", width: "600px" }}>
+      <Line data={dataset} options={{ animation: { duration: 300 } }} redraw />
+      <button
+        onClick={() => {
+          setXCount((count) => count + 1);
+          setData({
+            x: [...data.x, xCount],
+            y: [...data.y, faker.datatype.number(100)],
+          });
+        }}
+      >
+        Add data
+      </button>
+      <button
+        onClick={() => {
+          setXCount((count) => count - 1);
+          let tmpX = [...data.x];
+          tmpX.splice(-1);
+          let tmpY = [...data.y];
+          tmpY.splice(-1);
+          setData({
+            x: [...tmpX],
+            y: [...tmpY],
+          });
+        }}
+      >
+        Remove last sample
+      </button>
     </div>
   );
 };
